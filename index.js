99191b8dad26d99285c3dbb5e69188430f8686c3
huyansheng <huyansheng@didichuxing.cn>
2018-07-28 21:00:27 +0800

    add heroku support

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var compression = require('compression');
+var connectHistoryApiFallback = require('connect-history-api-fallback');
+
+var port = process.env.PORT || 6773;
+
+var app = express();
+
+app.use(compression());
+app.use(connectHistoryApiFallback());
+
+app.use('/', express.static('./dist'));
+
+// Listen
+app.listen(port, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('running in the ' + port);
+  }
+});
