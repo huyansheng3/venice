f3ab920b70d2a7f6a8ad2a16e20c7b3b9df1ce42
huyansheng <huyansheng@didichuxing.cn>
2018-08-11 10:39:44 +0800

    build

--- dist/static/js/0.d613c389cd9fc4ca6d81.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0],{gcei:function(e,t,o){"use strict";t.a={methods:{goBack:function(){window.history.length>1?this.$router.go(-1):this.$router.push("/")}},computed:{hideHeader:function(){var e=this.$route.query.hideHeader;return Boolean(Number(e))}}}}});
